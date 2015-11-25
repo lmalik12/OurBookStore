c4ac2f526c5bb16325685ce630717e8243c94a22
LMalik <lovedeepmalik@gmail.com>
2015-11-25 14:44:19 -0800

    assignment 5, works up until part2

--- assignment5/index.js
+++ assignment5/index.js
@@ -6,34 +6,57 @@ app.use(express.static(__dirname + '/public'))
 
 var MongoClient = require('mongodb').MongoClient;
 var db;
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017/collection';
 
 // Initialize connection once
-MongoClient.connect(url, function(err, database) { 
-  if (err) throw err;
-  else {
-    db = database;
-    console.log("Connected correctly to server.");
+  MongoClient.connect(url, function(err, database) { 
 
-    db.createCollection('products', {strict:true}, function(err, collection){
-      if (err) {
-        console.log(err);
-      };
-    });
+    if(err){ 
+      throw err;
+      console.log("Error cannot connect to Mongo Client");
+    } else {
+      db = database;
+
+      // populate tables with data
+      populateTables(db, function(){ });
+
+      console.log("Connected correctly to server.");
+    }
+  }); 
+
+app.get('/products', function(request, response) {
+  console.log("HEY KELLY");
+
+  var products = db.collection('products');
+  var cursor = products.find().toArray(function (err, data){
+               response.json(data);
+        });
+}); 
+
+var populateTables = function(){
+  var collection = db.collection('products');
+  collection.count(function (err, count) {
+      if (!err && count === 0) { // if collection is empty, populate db with the following collections
+
+           db.createCollection('products', {strict:true}, function(err, collection){
+              if (err) {
+                console.log("products collection error" + err);
+              };
+            });
     
-    db.createCollection('users', {strict:true}, function(err, collection){
-      if (err) {
-        console.log(err);
-      } 
-    });
-    
-    db.createCollection('orders', {strict:true}, function(err, collection){
-      if (err) {
-        console.log(err);
-      } 
-    });
-    
-    var p = [
+          db.createCollection('users', {strict:true}, function(err, collection){
+            if (err) {
+              console.log("users collection error" + err);
+            } 
+          });
+          
+          db.createCollection('orders', {strict:true}, function(err, collection){
+            if (err) {
+              console.log("orders collection error" + err);
+            } 
+          });
+
+             var p = [
       {name:'KeyboardCombo', price:40, quantity:20, image:'https://cpen400a.herokuapp.com/images/KeyboardCombo.png'},
       {name:'Mice', price:20, quantity:20, image:'https://cpen400a.herokuapp.com/images/Mice.png'},
       {name:'PC1', price:350, quantity:20, image:'https://cpen400a.herokuapp.com/images/PC1.png'},
@@ -46,59 +69,25 @@ MongoClient.connect(url, function(err, database) {
       {name:'Clothes2', price:30, quantity:20, image:'https://cpen400a.herokuapp.com/images/Clothes2.png'},
       {name:'Jeans', price:20, quantity:20, image:'https://cpen400a.herokuapp.com/images/Jeans.png'},
       {name:'Keyboard', price:30, quantity:20, image:'https://cpen400a.herokuapp.com/images/Keyboard.png'}
-    ];
+    ];  
 
-    var collection = db.collection('products');
-    collection.count(function (err, count) {
-      if (!err && count === 0) { // if collection is empty, populate db
         collection.insert(p, {w:1}, function(err, result) {
-          if (err) console.log(err); 
-          else {
+          if (err){ 
+            console.log(" problem inserting into collection" + err); 
+          }else {
             console.log("Inserted %d documents into the 'products' collection. ", result.length);
           }
         });
       }
     });
+}; 
 
-    // Start the application after the database connection is ready
-    app.listen(app.get('port'), function() {
-      console.log("Node app is running at localhost:" + app.get('port'));
-    });
-  }
+var getData = function(db, callback){
+     
+}
+ // Start the application after the database connection is ready
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '../index.html'));
-});
-
-var products;
-app.get('/products', function(request, response) {
-
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  var temp = {};
-  var collection = db.collection('products');
-  var cursor = collection.find({}, {_id:0});
-  cursor.each(function(err, doc) {
-    if (err) {
-      console.log(err);    
-    } else if (doc) {
-      console.log(doc);
-      var val = {
-        price : doc.price,
-        quantity : doc.quantity,
-        url : doc.image
-      };
-      temp[doc.name] = val;
-      console.log(temp);
-    } else {
-      products = temp; // after reading all documents, pass it to 'products' variable
-    }
-  }); 
-  
-  if (!products) // if 'products' object is empty, send an error message 
-    response.status(500).send("An error occurred, please try again"); 
-  else 
-    response.json(products);
-});
+   
